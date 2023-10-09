728f10ec115b09aff685f37ea9888a150e4eb7ad
ence1ad <romanrusl@mail.ru>
2023-10-10 02:02:50 +0300

    Integrate Redis for caching and storage
    In this commit, i've made several updates to enhance our application's performance and functionality:
    
    1. Added a `RedisSettings` class to `config.py` to manage Redis configuration settings, including host, port, database, and password.
    2. Implemented a `cache_url` property in the `RedisSettings` class to generate the connection URL for Redis.
    3. Initialized a Redis client (`redis_client`) using the settings from `cache_url` within the main function.
    4. Utilized the `RedisStorage` class to create a Redis-based storage solution for our application.
    
    Additionally, i've updated the `docker-compose.yml` file to include a Redis container:
    
    - The Redis container is named `redis_db` and uses the official Redis image.
    - A health check is configured to ensure Redis is running correctly.
    - The Redis server is configured with specific options, including maximum memory, persistence settings, and log level.
    - Redis data is persisted by mapping the `/data` volume to a local directory (`./redis_data`).
    
    These changes enable us to use Redis for caching and storage, enhancing our application's speed and data management capabilities.

--- config.py
+++ config.py
@@ -3,6 +3,24 @@ from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class LoggingSettings(BaseSettings):
+    """
+    Logging Settings
+    """
+    LEVEL: int
+    FORMAT: str
+
+
+class RedisSettings(BaseSettings):
+    """
+    Redis Settings
+    """
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_DB: int
+    REDIS_PASSWORD: str
+
+
 class TgBotSettings(BaseSettings):
     """
     Telegram Bot Settings
@@ -12,16 +30,18 @@ class TgBotSettings(BaseSettings):
     GROUP_ID: int
 
 
-class LoggingSettings(BaseSettings):
-    """
-    Logging Settings
-    """
-    LEVEL: int
-    FORMAT: str
-
-
-class Settings(TgBotSettings, LoggingSettings, BaseSettings):
+class Settings(LoggingSettings, RedisSettings, TgBotSettings,  BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+
+    @property
+    def cache_url(self) -> str:
+        """
+        The cache_url function returns a string that is the URL for connection to the Redis database.
+
+        :param self: Access the class attributes
+        :return: A string containing the connection url for a redis database
+        """
+        return f"redis://:{self.REDIS_PASSWORD}@{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}?protocol=3"
 
 
 @lru_cache
--- tgbot/__main__.py
+++ tgbot/__main__.py
@@ -1,6 +1,10 @@
 import asyncio
 import logging
 
+import redis.asyncio as redis
+from redis.asyncio import Redis
+
+from aiogram.fsm.storage.redis import RedisStorage
 from aiogram import Dispatcher, Bot
 
 from config import settings
@@ -10,9 +14,14 @@ from tgbot.handlers import register_common_handlers
 async def main() -> None:
     # Initialize bot
     bot: Bot = Bot(settings.BOT_TOKEN, parse_mode='html')
+    # Initialize redis
+    redis_client: Redis = redis.from_url(settings.cache_url)
+
+    # Initialize redis storage
+    storage: RedisStorage = RedisStorage(redis=redis_client)
 
     # Initialize dispatcher
-    dp: Dispatcher = Dispatcher()
+    dp: Dispatcher = Dispatcher(storage=storage)
 
     # Register handlers
     common_handlers_router = register_common_handlers()
