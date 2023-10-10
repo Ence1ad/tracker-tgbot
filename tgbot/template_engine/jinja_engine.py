c7439ba91665c525e370b518b755dd7796945f94
ence1ad <romanrusl@mail.ru>
2023-10-10 21:09:46 +0300

    Add template rendering functionality using Jinja2
    This commit introduces a new feature to the project by adding a template rendering engine using Jinja2. The following changes have been made:
    
    1. **New Template Rendering Module**:
       - Created a new module named `jinja_engine.py` to handle template rendering.
    
    2. **Template Environment Configuration**:
       - Defined a template path and configured the Jinja2 environment with a `FileSystemLoader` to load templates from a specified directory.
       - Configured autoescaping for HTML templates.
    
    3. **Render Template Function**:
       - Added a `render_template` function that takes the name of the template, values, and keyword arguments for rendering.
       - The function fetches the template, renders it with the provided values or keyword arguments, and returns the rendered template as a string.
    
    This feature allows for the easy rendering of templates, making it simpler to generate dynamic content within the project.

---
+++ tgbot/template_engine/jinja_engine.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from typing import Optional, Any, Dict
+
+from jinja2 import Environment, select_autoescape, Template, FileSystemLoader
+
+
+path = 'tgbot/template_engine/templates'
+env = Environment(
+    loader=FileSystemLoader(Path(path)),
+    autoescape=select_autoescape(['html'])
+)
+
+
+def render_template(name: str, values: Optional[Dict[str, Any]] = None,
+                    **kwargs: dict) -> str:
+    """
+    Renders template & returns text
+
+    :param name: Name of template
+    :param values: Values for template (optional)
+    :param kwargs: Keyword-arguments for template (high-priority)
+    """
+    template: Template = env.get_template(name)
+    if values:
+        rendered_template: str = template.render(values, **kwargs)
+    else:
+        rendered_template: str = template.render(**kwargs)
+    return rendered_template
