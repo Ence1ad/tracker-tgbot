bcf31ed6f43542480534c7a141dfe9bb09c4bb04
ence1ad <romanrusl@mail.ru>
2023-10-11 14:25:14 +0300

    Update PostgreSQL Initialization Script and Docker Compose Service
    In this commit, I've made the following changes to enhance the PostgreSQL setup:
    
    1. **`init-user-db.sh` Script**:
       - Added a script `init-user-db.sh` that configures PostgreSQL settings, including max connections, shared buffers, effective cache size, and more, to optimize performance.
       - Created a new user with the provided credentials and granted it necessary privileges.
    
    2. **Docker Compose Service**:
       - Modified the Docker Compose service configuration to include the `init-user-db.sh` script as an initialization step for the PostgreSQL container.
    
    These changes improve the setup and initialization process for the PostgreSQL database, ensuring that it's configured with optimal settings and that the required user and privileges are created.

---
+++ container_conf/postgres/init-user-db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+ALTER SYSTEM SET max_connections = 50;
+ALTER SYSTEM SET shared_buffers = '1GB';
+ALTER SYSTEM SET effective_cache_size = '4GB';
+ALTER SYSTEM SET work_mem = '16MB';
+ALTER SYSTEM SET maintenance_work_mem = '512MB';
+ALTER SYSTEM SET random_page_cost = 1.1;
+ALTER SYSTEM SET temp_file_limit = '10GB';
+ALTER SYSTEM SET log_min_duration_statement = '200ms';
+ALTER SYSTEM SET idle_in_transaction_session_timeout = '10s';
+ALTER SYSTEM SET lock_timeout = '1s';
+ALTER SYSTEM SET statement_timeout = '60s';
+
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+ALTER SYSTEM SET shared_preload_libraries = pg_stat_statements;
+
+CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASS';
+GRANT CONNECT ON DATABASE $POSTGRES_DB TO $DB_USER;
+GRANT ALL ON SCHEMA public TO $DB_USER;
+
+EOSQL
