6bcabd5c6146c55adcbbe9bc14be8446b256108a
ence1ad <romanrusl@mail.ru>
2023-10-14 15:31:24 +0300

    Add Redis Utility Functions for Language Management
    In this commit, two new modules have been added to the project's cache directory: `redis_utils.py` and `language_redis_manager.py`. These modules include utility functions for efficient Redis-based language management. The functions added are:
    
    In `redis_utils.py`:
    1. `set_redis_name(user_id, prefix='')`: Generates a unique Redis key name for a user based on their ID and an optional prefix.
    
    In `language_redis_manager.py`:
    2. `redis_hget_lang(user_id, redis_client, local=None)`: Retrieves the language code of a user from Redis, falling back to a default global language code if not found.
    3. `redis_hset_lang(user_id, redis_client, lang_code)`: Sets the language code for a user in Redis.
    
    These utility functions provide a robust language management system using Redis, enhancing the project's language-related functionality and performance.

---
+++ cache/language_redis_manager.py
@@ -0,0 +1,48 @@
+from redis.asyncio import Redis
+
+from cache.redis_utils import set_redis_name
+from config import settings
+
+LANG_PREFIX = 'lang'
+
+
+async def redis_hget_lang(user_id: int,  redis_client: Redis, local: str | None = None
+                          ) -> str:
+    """
+    Retrieve the language code for a user from Redis.
+
+    This function fetches the language code associated with a user from a Redis database.
+    If no language code is found, it returns the default global language code or the provided local code.
+
+    :param user_id: int: The unique identifier of the user.
+    :param redis_client: Redis: An instance of a Redis client.
+    :param local: str | None: An optional local language code (default is None).
+    :return: str: The language code for the user or the local language code if specified.
+    """
+    lang_name: str = set_redis_name(user_id, LANG_PREFIX)
+    lang_code: bytes | str = await redis_client.hget(name=lang_name, key=str(user_id))
+    if isinstance(lang_code, bytes):
+        lang_code = lang_code.decode(encoding='utf-8')
+        return lang_code
+    if not lang_code and local is not None:
+        return local
+    else:
+        return settings.EN_LANG_CODE
+
+
+async def redis_hset_lang(user_id: int,  redis_client: Redis, lang_code: str | bytes
+                          ) -> int:
+    """
+    Set the language code for a user in Redis.
+
+    This function is used to update the language code for a user in a Redis database.
+
+    :param user_id: int: The unique identifier of the user.
+    :param redis_client: Redis: An instance of a Redis client.
+    :param lang_code: str | bytes: The language code to set for the user.
+    :return: int: The number of fields that were changed (0 if no changes occurred).
+    """
+    if lang_code is not None:
+        lang_name: str = set_redis_name(user_id, LANG_PREFIX)
+        return await redis_client.hset(name=lang_name, key=str(user_id),
+                                       value=lang_code)
