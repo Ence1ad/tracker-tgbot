5bddd4de212a6f131c1edae9906f2a02d492a3d6
ROMAN AKHMETKHANOV <36819965+Ence1ad@users.noreply.github.com>
2023-10-14 16:52:49 +0300

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,4 +1,4 @@
-from .users.user_model import UserModel
+from .models.user_model import UserModel
 from .categories.categories_model import CategoriesModel
 from .actions.actions_models import ActionsModel
 from .tracker.tracker_model import TrackerModel
