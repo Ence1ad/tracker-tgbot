b680191c40614fea0c696c64747f5c61928c8972
ence1ad <romanrusl@mail.ru>
2023-10-17 00:43:44 +0300

    feat(db): include new models in __init__.py
    This commit updates the `db/__init__.py` file to include three new models: 'CategoryModel,' 'ActionModel,' and 'TrackerModel.' This change ensures that these models are part of the exported symbols in the package.

--- db/__init__.py
+++ db/__init__.py
@@ -1,3 +1,6 @@
 from db.models.user_model import UserModel
+from db.models.category_model import CategoryModel
+from db.models.action_model import ActionModel
+from db.models.tracker_model import TrackerModel
 
-__all__ = ['UserModel']
+__all__ = ['UserModel', 'CategoryModel', 'ActionModel', 'TrackerModel']
