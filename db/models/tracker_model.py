c059d1275e62f20ebce2751ed113a1d430abc22e
ence1ad <romanrusl@mail.ru>
2023-10-17 00:39:59 +0300

    feat(db): add TrackerModel for database representation
    This commit introduces the `TrackerModel` class, which represents tracking records in the database. The model includes fields for `tracker_id`, `duration`, `track_start`, `track_end`, `category_id`, `action_id`, and `user_id`, along with appropriate constraints to ensure data integrity. The associated table name is 'trackers,' and an index is added to improve query performance.

---
+++ db/models/tracker_model.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta
+
+from sqlalchemy import ForeignKey, DateTime, Identity, Index, BigInteger
+
+from sqlalchemy.orm import mapped_column, Mapped
+
+from db.base_model import AsyncSaBase
+
+
+class TrackerModel(AsyncSaBase):
+    """Represents a tracking record in the database.
+
+    :cvar __tablename__: The name of the database table for tracking records.
+    :cvar tracker_id: The unique identifier for the tracking record (primary key).
+    :cvar duration: The duration of the tracking record.
+    :cvar track_start: The timestamp when tracking started (not nullable).
+    :cvar track_end: The timestamp when tracking ended.
+    :cvar category_id: The identifier of the associated category (not nullable).
+    :cvar action_id: The identifier of the associated action (not nullable).
+    :cvar user_id: The identifier of the associated user (not nullable).
+
+    - `__tablename__`: 'trackers' - The name of the table.
+    - `tracker_id`: Unique identifier for the tracking record (primary key).
+    - `duration`: The duration of the tracking record.
+    - `track_start`: The timestamp when tracking started (not nullable).
+    - `track_end`: The timestamp when tracking ended.
+    - `category_id`: The identifier of the associated category (not nullable).
+    - `action_id`: The identifier of the associated action (not nullable).
+    - `user_id`: The identifier of the associated user (not nullable).
+
+    This class represents tracking records in the database, including their duration,
+    start and end timestamps, category, action, and user associations.
+
+    :func __str__: Return a string representation of the tracker's unique identifier.
+
+    :rtype: str
+    """
+
+    __tablename__ = 'trackers'
+    tracker_id: Mapped[int] = mapped_column(Identity(), primary_key=True)
+    duration: Mapped[timedelta] = mapped_column(nullable=True)
+    track_start: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, default=datetime.now
+    )
+    track_end: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=True, onupdate=datetime.now
+    )
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey('categories.category_id', ondelete='cascade'), nullable=False
+    )
+    action_id: Mapped[int] = mapped_column(
+        ForeignKey('actions.action_id', ondelete='cascade'), nullable=False
+    )
+    user_id: Mapped[int] = mapped_column(
+        BigInteger, ForeignKey('users.user_id', ondelete='cascade'), nullable=False
+    )
+
+    __table_args__ = (
+        Index('trackers_user_id_tracker_id_idx', 'user_id', 'tracker_id')
+    )
+
+    def __str__(self) -> str:
+        return str(self.tracker_id)
