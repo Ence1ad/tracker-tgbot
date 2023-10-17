114dbe6972e2f40735647c6dea0bb5bdea9f227d
ence1ad <romanrusl@mail.ru>
2023-10-17 23:33:45 +0300

    feat: introduce help handler and templates
    Introduce a help handler and template files for providing user assistance in the Telegram bot. The changes include:
    
    - `tgbot/handlers/common_handles/help_handler.py`: Implementation of the `command_help_handler` function for handling user requests for help. It provides information about available commands based on the user's input.
    - Template files under `tgbot/template_engine/templates/`: Introduction of template files for help messages in both English (en) and Russian (ru) languages. These templates cover various topics, including main commands, categories, actions, trackers, and reports.
    
    These changes enhance the user experience by providing informative help messages in multiple languages and organizing the code for help-related functionality.

---
+++ tgbot/handlers/common_handles/help_handler.py
@@ -0,0 +1,71 @@
+from aiogram.types import Message
+from fluentogram import TranslatorRunner
+from redis.asyncio import Redis
+
+from cache.language_redis_manager import redis_hget_lang
+from config import settings
+from tgbot.template_engine.jinja_engine import render_template
+
+
+async def command_help_handler(message: Message, redis_client: Redis,
+                               i18n: TranslatorRunner) -> Message:
+    """Handle the user's request for help by providing info about available commands.
+
+    :param message: Message: The Message object representing the user's request.
+    :param redis_client: Redis: The Redis client for data storage.
+    :param i18n: TranslatorRunner: An internationalization runner for text localization.
+    :return: Message: A response message containing information about available
+    commands.
+    """
+    user_id: int = message.from_user.id
+    current_user_lang = await redis_hget_lang(user_id=user_id,
+                                              redis_client=redis_client)
+
+    if current_user_lang == settings.RU_LANG_CODE:
+        lang_code: dict[str, str] = {
+            "main": 'ru_help_main.html',
+            "cat": 'ru_help_categories.html',
+            "act": 'ru_help_actions.html',
+            "tra": 'ru_help_trackers.html',
+            "rep": 'ru_help_reports.html'
+        }
+    else:
+        lang_code = {
+            "main": 'en_help_main.html',
+            "cat": 'en_help_categories.html',
+            "act": 'en_help_actions.html',
+            "tra": 'en_help_trackers.html',
+            "rep": 'en_help_reports.html'
+        }
+
+    # Get the text of the user's message
+    text = message.text
+
+    # Split the message text into words
+    words = text.split('_')
+    # If there is only one word (i.e., just "/help" without specifying a command)
+    if len(words) == 1:
+        # Provide a general help message
+        general_help_msg: str = render_template(lang_code.get("main"))
+        return await message.reply(text=general_help_msg)
+    elif len(words) == 2:
+        # Extract the <command> part from the message
+        command = words[1]
+
+        if command == "cat":
+            categories_help_msg: str = render_template(lang_code.get("cat"))
+            return await message.reply(text=categories_help_msg)
+        elif command == "act":
+            actions_help_msg: str = render_template(lang_code.get("act"))
+            return await message.reply(text=actions_help_msg)
+        elif command == "tra":
+            trackers_help_msg: str = render_template(lang_code.get("tra"))
+            return await message.reply(text=trackers_help_msg)
+        elif command == "rep":
+            reports_help_msg: str = render_template(lang_code.get("rep"))
+            return await message.reply(text=reports_help_msg)
+        else:
+            return await message.reply(text=i18n.get('help_unknown_command_text'))
+    else:
+        # Handle cases where the user entered an invalid command format
+        return await message.reply(text=i18n.get('help_invalid_command_format_text'))
