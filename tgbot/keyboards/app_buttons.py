ca5b97aed5e47715120ed386c07558b680208da6
ence1ad <romanrusl@mail.ru>
2023-10-17 20:26:53 +0300

    fix(app_buttons.py): improve AppButtons data class
    Modified the 'AppButtons' data class in the 'app_buttons.py' file to improve the initialization and use of button Enum classes. The changes include:
    
    - Importing Enum classes (CategoriesButtonsData, GeneralButtonsData, ActionsButtonsData, TrackersButtonsData, ReportsButtonsData, and SettingsButtonsData) from 'buttons_classes.py'.
    - Updating the class to use these Enum classes for button definitions, improving code readability and organization.
    - Creating a 'settings_btn_list' containing keys of 'SettingsButtonsData' Enum members for easy reference.
    
    These changes enhance the 'AppButtons' class's maintainability and readability by using Enum classes for button definitions and simplifying the handling of setting buttons.

--- tgbot/keyboards/app_buttons.py
+++ tgbot/keyboards/app_buttons.py
@@ -1,9 +1,18 @@
 from dataclasses import dataclass
 
+from tgbot.keyboards.buttons_classes import CategoriesButtonsData, GeneralButtonsData,\
+    ActionsButtonsData, TrackersButtonsData, ReportsButtonsData, SettingsButtonsData
+
 
 @dataclass
 class AppButtons:
-    """Initialize the AppButtons data class.
+    """Initialize the AppButtons data class."""
 
-    This data class provides a collection of predefined buttons
-    """
+    categories_btn_source: type[CategoriesButtonsData] = CategoriesButtonsData
+    general_btn_source: type[GeneralButtonsData] = GeneralButtonsData
+    actions_btn_source: type[ActionsButtonsData] = ActionsButtonsData
+    trackers_btn_source: type[TrackersButtonsData] = TrackersButtonsData
+    reports_btn_source: type[ReportsButtonsData] = ReportsButtonsData
+    settings_btn_source: type[SettingsButtonsData] = SettingsButtonsData
+    settings_btn_list = list(SettingsButtonsData.__members__.keys())
+
