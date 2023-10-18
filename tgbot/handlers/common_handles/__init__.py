ac44adf264332494db288c61072ae6db67d84570
ence1ad <romanrusl@mail.ru>
2023-10-18 19:07:55 +0300

    feat(handlers): register and enhance common handlers
    This commit enhances the common handlers in the 'handlers/common_handles' package by adding new functionality and registering additional handlers. The changes include the following:
    
    - `command_cancel_handler` and `callback_cancel_handler` have been added to allow users to cancel ongoing operations. These handlers are now registered and can be invoked with the '/cancel' command.
    
    - `exit_menu_handler` has been introduced to handle user requests to exit the current menu or operation. It allows users to return to the main menu or exit an ongoing process.
    
    - `command_help_handler` is now registered and handles user requests for help, providing information about available commands.
    
    - `command_settings_handler` has been added to respond to users' requests to access settings, providing a settings menu.
    
    - Language settings, including options to change the language, have been added. Users can select their preferred language, and the 'set_user_lang' handler sets the language based on user choice.
    
    - Various handlers are registered to support these new features, such as `language_settings` and `set_user_lang`.
    
    These enhancements improve user interaction with the bot by providing options to manage ongoing operations, access settings, and select language preferences. The additional functionality enhances the overall user experience.

--- tgbot/handlers/common_handles/__init__.py
+++ tgbot/handlers/common_handles/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Router, F
-from aiogram.filters import Command, ChatMemberUpdatedFilter, JOIN_TRANSITION,\
+from aiogram.filters import Command, ChatMemberUpdatedFilter, JOIN_TRANSITION, \
     LEAVE_TRANSITION
+from aiogram.fsm.state import any_state
 
 from config import settings
 
@@ -20,7 +21,14 @@ def register_common_handlers() -> Router:
 
     :return: A configured Router object with the registered handlers.
     """
+    from tgbot.keyboards.app_buttons import AppButtons
+    from tgbot.utils.bot_commands import CommandName
     from .start_handler import command_start_handler
+    from .cancel_handler import command_cancel_handler, callback_cancel_handler
+    from .exit_handler import exit_menu_handler
+    from .help_handler import command_help_handler
+    from .settings_handler import command_settings_handler, language_settings, \
+        set_user_lang
     from .user_group_status import add_user_handler, remove_user_handler
 
     router = Router()
@@ -36,5 +44,23 @@ def register_common_handlers() -> Router:
     )
 
     router.message.register(command_start_handler, Command('start', ignore_case=True))
+    router.message.register(command_cancel_handler,
+                            Command(CommandName.CANCEL.name, ignore_case=True),
+                            any_state)
+    router.callback_query.register(
+        callback_cancel_handler,
+        F.data == AppButtons.general_btn_source.CANCEL_BTN.name, any_state
+    )
+    router.callback_query.register(
+        exit_menu_handler, F.data == AppButtons.general_btn_source.EXIT_BTN.name
+    )
+    router.message.register(command_help_handler, F.text.startswith('/help'))
+    router.message.register(command_settings_handler,
+                            Command(CommandName.SETTINGS.name, ignore_case=True))
+    router.callback_query.register(
+        language_settings, F.data == AppButtons.settings_btn_source.LANGUAGE.name
+    )
+    router.callback_query.register(set_user_lang,
+                                   (F.data.in_(AppButtons.settings_btn_list)))
 
     return router
