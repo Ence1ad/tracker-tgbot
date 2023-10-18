f4c2d31046fd95ad06f4dbe5a878c5ed0e96503c
ence1ad <romanrusl@mail.ru>
2023-10-18 20:11:56 +0300

    test: Add testing utility functions for message, chat, callback query, and update
    This commit introduces a set of utility functions in the 'utils.py' file within the 'tests/integration/test_bot' directory. These functions are designed to facilitate the creation of testing objects for various components of the Telegram bot, including messages, chats, callback queries, and updates. The key features and changes in this commit include:
    
    - 'get_message': A utility function for creating message objects for testing. Users can specify the text, chat, sender, and additional attributes for the message.
    
    - 'get_chat': A utility function for creating chat objects for testing. Users can specify the chat's unique identifier, type, title, username, and other attributes.
    
    - 'get_callback_query': A utility function for creating callback query objects for testing. Users can specify the data, user, associated message, and additional attributes.
    
    - 'get_update': A utility function for creating mocked update objects for testing. Users can specify the associated message, callback query, and additional attributes.
    
    These utility functions simplify the process of creating test objects for the Telegram bot's components, making it easier to write and maintain test cases for various bot features. They enhance the testing infrastructure, ensuring comprehensive testing coverage and quality assurance.
    
    The new utility functions are intended to support testing efforts and contribute to the overall reliability and quality of the Telegram bot codebase.

---
+++ tests/integration/test_bot/utils.py
@@ -0,0 +1,121 @@
+from datetime import datetime
+
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import CallbackQuery, Chat, Message, Update, User
+
+from tests.utils import MAIN_USER_ID
+
+TEST_USER = User(
+    id=MAIN_USER_ID,
+    is_bot=False,
+    first_name='Test'
+)
+
+TEST_CHAT = Chat(
+    id=12,
+    type='private',
+    username=TEST_USER.username,
+    first_name=TEST_USER.first_name,
+    last_name=TEST_USER.last_name,
+)
+
+TEST_MESSAGE = Message(message_id=123, date=datetime.now(), chat=TEST_CHAT, text='')
+
+
+def get_message(text: str, chat=TEST_CHAT, from_user=None, **kwargs) -> Message:
+    """Create a message object for testing.
+
+    Args:
+        text (str): The text of the message.
+        chat (Chat): The chat to associate with the message.
+        from_user (User, optional): The user who sent the message.
+        **kwargs: Additional message attributes.
+
+    Returns:
+        Message: A message object for testing.
+    """
+    return Message(
+        message_id=123,
+        date=datetime.now(),
+        chat=chat,
+        from_user=from_user,
+        sender_chat=TEST_CHAT,
+        text=text,
+        **kwargs
+    )
+
+
+def get_chat(
+    chat_id: int = None,
+    chat_type: str = 'private',
+    title: str = 'TEST_TITLE',
+    username: str = TEST_CHAT.username,
+    **kwargs
+) -> Chat:
+    """Create a chat object for testing.
+
+    Args:
+        chat_id (int, optional): The chat's unique identifier.
+        chat_type (str, optional): The type of chat, e.g., 'private'.
+        title (str, optional): The title of the chat.
+        username (str): The username associated with the chat.
+        **kwargs: Additional chat attributes.
+
+    Returns:
+        Chat: A chat object for testing.
+    """
+    return Chat(
+        id=chat_id,
+        type=chat_type,
+        title=title,
+        username=username,
+        first_name=TEST_USER.first_name,
+        last_name=TEST_USER.last_name,
+        **kwargs
+    )
+
+
+def get_callback_query(
+    data: str | CallbackData, from_user=None, message=None, **kwargs
+) -> CallbackQuery:
+    """Create a callback query object for testing.
+
+    Args:
+        data (str or CallbackData): The data associated with the query.
+        from_user (User, optional): The user who sent the query.
+        message (Message, optional): The message associated with the query.
+        **kwargs: Additional callback query attributes.
+
+    Returns:
+        CallbackQuery: A callback query object for testing.
+    """
+    return CallbackQuery(
+        id='test',
+        from_user=from_user,
+        chat_instance='test',
+        message=message or TEST_MESSAGE,
+        data=data,
+        **kwargs
+    )
+
+
+def get_update(
+    message: Message = None, callback_query: CallbackQuery = None, **kwargs
+) -> Update:
+    """Create a mocked update object for testing.
+
+    Args:
+        message (Message, optional): The message associated with the update.
+        callback_query (CallbackQuery, optional): The callback query associated with
+         the update.
+        **kwargs: Additional update attributes.
+
+    Returns:
+        Update: A mocked update object for testing.
+    """
+    return Update(
+        update_id=187,
+        message=message,
+        callback_query=callback_query,
+        **kwargs
+    )
