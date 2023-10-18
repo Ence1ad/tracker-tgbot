b52889e255c88117da388a18df1824cb97f1c7e9
ence1ad <romanrusl@mail.ru>
2023-10-18 18:40:12 +0300

    feat(handlers): enhance /start command handler with tracker information
    This commit introduces improvements to the '/start' command handler within the 'start_handler.py' file. The handler now provides the following functionality:
    
    - It handles the '/start' command and clears the user's finite state machine (FSM) context, ensuring a fresh start.
    - The main menu with relevant buttons is displayed to the user.
    - It checks whether the user has a running tracker and provides information if one is active.
    - Tracker information, including the title, category, selected action, and duration, is displayed to the user when a tracker is running. This feature enhances user awareness of their tracking activities.
    
    These enhancements improve the user experience by providing useful information when starting the bot. It also ensures that the user begins with a clean slate by clearing the FSM context.

--- tgbot/handlers/common_handles/start_handler.py
+++ tgbot/handlers/common_handles/start_handler.py
@@ -1,11 +1,46 @@
-from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, InlineKeyboardMarkup
+from fluentogram import TranslatorRunner
+from redis.asyncio import Redis
+
+from cache.trackers_redis_manager import is_redis_hexists_tracker
+from tgbot.keyboards.app_buttons import AppButtons
+from tgbot.keyboards.inline_kb import menu_inline_kb
+from tgbot.utils.tracker_info import started_tracker_info
 
 
-async def command_start_handler(message: Message) -> Message:
-    """Handle the /start command.
+async def command_start_handler(
+        message: Message, redis_client: Redis, buttons: AppButtons, state: FSMContext,
+        i18n: TranslatorRunner
+) -> Message:
+    """Handle the /start command and display the main menu to the user.
 
-    :param message:
-    :return:Message
+    Note:
+    ----
+        This function handles the /start command, clears the user's FSM state, and
+        displays the main menu.
+        It also checks if the user has a started tracker and provides information if
+        applicable.
+
+    :param message: Message: The Message object representing the user's message.
+    :param redis_client: Redis: The Redis client for data storage.
+    :param buttons: AppButtons: An instance of AppButtons for button configuration.
+    :param state: FSMContext: The finite state machine (FSM) context for the user.
+    :param i18n: TranslatorRunner: An internationalization runner for text localization.
+    :return: Message: The response message to be sent to the user.
     """
-    return await message.answer('Start message handler')
-
+    user_id: int = message.from_user.id
+    await message.delete()
+    await state.clear()
+    markup: InlineKeyboardMarkup = await menu_inline_kb(
+        await buttons.general_btn_source.main_menu_buttons(), i18n, is_cancel=True
+    )
+    if await is_redis_hexists_tracker(user_id, redis_client):
+        tracker_info: str = await started_tracker_info(
+            user_id=user_id, redis_client=redis_client, title='started_tracker_title',
+            i18n=i18n,
+        )
+        return await message.answer(text=tracker_info, reply_markup=markup)
+    else:
+        return await message.answer(text=i18n.get('user_in_db_text'),
+                                    reply_markup=markup)
