f9d03fc15e6533abfbac258f796a73f2e5cec6ae
ence1ad <romanrusl@mail.ru>
2023-10-18 20:18:49 +0300

    test: Add pytest-asyncio fixtures and setup for bot and dispatcher testing
    This commit enhances the testing infrastructure for the Telegram bot by introducing new pytest-asyncio fixtures and setting up key components for testing, such as the bot, dispatcher, Redis storage, and more. The significant changes and additions in this commit include:
    
    - Introduction of a new pytest-asyncio fixture 'redis_storage' that creates a Redis storage object for use in testing. This fixture helps ensure that the Redis storage environment is available for the tests.
    - Addition of fixtures for 'bot', 'i18n', 'buttons', 'scheduler', and 'dispatcher'. These fixtures set up the bot instance, translation hub, application buttons, scheduler, and dispatcher, respectively. They help prepare the testing environment for various bot-related tests.
    - Configuration of middleware for the dispatcher, including 'DbSessionMiddleware', 'CacheMiddleware', 'SchedulerMiddleware', 'ButtonsMiddleware', and 'TranslatorRunnerMiddleware.' These middlewares facilitate interactions between the bot, Redis, and other components.
    - Registration of common handlers using the 'register_common_handlers' function within the dispatcher. This step ensures that the common handlers are available for testing.
    - Proper handling of bot session and dispatcher cleanup in the finalization stage to maintain a clean testing environment.
    
    The newly added pytest-asyncio fixtures and the overall setup of the bot and dispatcher for testing provide a robust foundation for writing and running integration tests for the Telegram bot. These improvements contribute to enhancing the reliability and quality of the bot's codebase and testing suite.

---
+++ tests/integration/test_bot/conftest.py
@@ -0,0 +1,163 @@
+import pytest
+import pytest_asyncio
+from aiogram import Dispatcher
+from aiogram.fsm.storage.base import StorageKey
+from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.types import User, Chat
+from cache.trackers_redis_manager import redis_hmset_create_tracker
+from config import settings
+from tests.integration.mocked_bot import MockedBot
+from tests.integration.test_bot.utils import get_update, get_callback_query, \
+    get_message, TEST_CHAT
+from tgbot.handlers import register_common_handlers
+from tgbot.keyboards.app_buttons import AppButtons
+from tgbot.localization.localize import Translator
+from tgbot.middlewares.db_middleware import DbSessionMiddleware
+from tgbot.middlewares.redis_middleware import CacheMiddleware
+from tgbot.middlewares.apscheduler_middleware import SchedulerMiddleware
+from tgbot.middlewares.button_middleware import ButtonsMiddleware
+from tgbot.middlewares.translation_middleware import TranslatorRunnerMiddleware
+from tgbot.schedule.schedule_adjustment import setup_scheduler
+
+
+@pytest_asyncio.fixture(scope='class')
+async def redis_storage(redis_url):
+    if not redis_url:
+        pytest.skip("Redis is not available here")
+    storage = RedisStorage.from_url(redis_url)
+    try:
+        await storage.redis.info()
+    except ConnectionError as e:
+        pytest.skip(str(e))
+    try:
+        yield storage
+    finally:
+        conn = await storage.redis
+        await conn.flushdb()
+        await storage.close()
+
+
+@pytest_asyncio.fixture()
+async def bot():
+    return MockedBot()
+
+
+@pytest.fixture()
+def i18n(lang_bot_settings):
+    hub = Translator()
+    return hub.t_hub.get_translator_by_locale(lang_bot_settings)
+
+
+@pytest.fixture()
+def buttons():
+    return AppButtons()
+
+
+@pytest_asyncio.fixture()
+async def scheduler(async_session_fixture, redis_storage, bot, redis_cli):
+    schedule = await setup_scheduler(
+        bot=bot, jobstores=settings.scheduler_job_stores, redis_client=redis_cli,
+        storage=redis_storage, async_session=async_session_fixture
+    )
+    return schedule
+
+
+@pytest_asyncio.fixture()
+async def dispatcher(bot, redis_cli, buttons, lang_bot_settings, i18n,
+                     async_session_fixture,  redis_storage, scheduler):
+    dp = Dispatcher(storage=redis_storage)
+    translator = Translator(global_lang=lang_bot_settings)
+    dp.update.middleware.register(DbSessionMiddleware(async_session_fixture))
+    dp.update.middleware.register(CacheMiddleware(redis_cli))
+    dp.callback_query.middleware.register(SchedulerMiddleware(scheduler))
+    dp.update.middleware.register(ButtonsMiddleware(buttons))
+    dp.update.middleware.register(TranslatorRunnerMiddleware(translator))
+
+    common_handlers_router = register_common_handlers()
+    dp.include_routers(common_handlers_router)
+    await dp.emit_startup()
+    try:
+        yield dp
+    finally:
+        await bot.session.close()
+        await dp.emit_shutdown()
+
+
+@pytest_asyncio.fixture()
+async def execute_callback_query_handler(bot: MockedBot, dispatcher: Dispatcher,
+                                         chat_fixt_fact):
+    async def get_handler_result(user_id: int, data, state=None):
+        user: User = User(id=user_id, first_name='test_user', is_bot=False)
+        chat: Chat = await chat_fixt_fact(user_id)
+        if state is not None:
+            context = dispatcher.fsm.get_context(bot=bot, chat_id=chat.id,
+                                                 user_id=user.id)
+            await context.set_state(state)
+        res = await dispatcher.feed_update(bot=bot, update=get_update(
+            callback_query=get_callback_query(data=data, from_user=user))
+                                           )
+        return res
+    return get_handler_result
+
+
+@pytest_asyncio.fixture()
+async def execute_message_handler(bot: MockedBot, dispatcher: Dispatcher,
+                                  chat_fixt_fact):
+    async def get_handler_result(user_id: int, text, state=None):
+        user: User = User(id=user_id, first_name='test_user', is_bot=False)
+        chat: Chat = await chat_fixt_fact(user_id)
+        if state is not None:
+            context = dispatcher.fsm.get_context(bot=bot, chat_id=chat.id,
+                                                 user_id=user.id)
+            await context.set_state(state)
+
+        res = await dispatcher.feed_update(bot=bot, update=get_update(
+            message=get_message(text=text, chat=chat, from_user=user))
+                                           )
+        return res
+    return get_handler_result
+
+
+@pytest_asyncio.fixture
+async def create_tracker_fixt_fact(redis_cli):
+    async def _create_tracker_fixt_fact(
+            user_id: int, category_id: int, category_name: str, action_id: int,
+            action_name: str, tracker_id: str
+    ):
+        tracker = await redis_hmset_create_tracker(
+            user_id=user_id, tracker_id=tracker_id, action_id=action_id,
+            action_name=action_name, category_id=category_id,
+            category_name=category_name, redis_client=redis_cli
+        )
+        return tracker
+    return _create_tracker_fixt_fact
+
+
+@pytest_asyncio.fixture
+async def chat_fixt_fact():
+    async def _create_chat(
+            chat_id: int = None, chat_type: str = 'private', title: str = 'TEST_TITLE',
+            username: str = TEST_CHAT.username, **kwargs
+    ) -> Chat:
+        return Chat(id=chat_id, type=chat_type, title=title, username=username,
+                    **kwargs)
+    return _create_chat
+
+
+@pytest_asyncio.fixture
+async def set_state_data_fict_fact(chat_fixt_fact, dispatcher, bot):
+    async def set_state_data(user_id, data):
+        chat: Chat = await chat_fixt_fact(user_id)
+        key = StorageKey(bot_id=bot.id, chat_id=chat.id, user_id=user_id)
+        return await dispatcher.fsm.storage.set_data(key=key, data=data)
+    return set_state_data
+
+
+@pytest_asyncio.fixture
+async def get_state_fict_fact(chat_fixt_fact, dispatcher, bot):
+    async def get_state(user_id):
+        chat: Chat = await chat_fixt_fact(user_id)
+        key = StorageKey(bot_id=bot.id, chat_id=chat.id, user_id=user_id)
+        state = await dispatcher.fsm.storage.get_state(key)
+        return state
+    return get_state
