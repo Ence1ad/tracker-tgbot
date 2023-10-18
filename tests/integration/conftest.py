007a227c3289c56ccc4aa875441abc8f22e8ee40
ence1ad <romanrusl@mail.ru>
2023-10-18 20:16:54 +0300

    test: Add pytest-asyncio fixture for creating a tracker object for testing
    This commit introduces a new pytest-asyncio fixture, 'create_tracker_fixt,' in the 'conftest.py' file within the 'tests/integration' directory. This fixture is designed to facilitate the creation of a tracker object for testing purposes. The key features and changes in this commit include:
    
    - 'create_tracker_fixt': A pytest-asyncio fixture used to create a tracker object for testing. This fixture interacts with the Redis database to set up the necessary tracker data. The created tracker object is used during testing, and it is associated with a specific user and certain parameters.
    
    - The fixture sets up the Redis data needed for a tracker, which includes user ID, tracker ID, action ID, action name, category ID, and category name.
    
    - The fixture ensures that the test environment is appropriately prepared for tracker-related testing.
    
    The introduction of this fixture streamlines the testing process for tracker-related functionality in the Telegram bot. It simplifies the creation and management of tracker objects for testing, ensuring that test cases can be executed more efficiently and accurately.
    
    This fixture is specifically designed to support the testing efforts related to trackers and contributes to the overall quality assurance and reliability of the Telegram bot codebase.

---
+++ tests/integration/conftest.py
@@ -0,0 +1,17 @@
+import pytest_asyncio
+
+from cache.trackers_redis_manager import redis_hmset_create_tracker,\
+    redis_delete_tracker
+from tests.utils import USER_ID_WITH_TRACKER_LIMIT
+
+
+@pytest_asyncio.fixture(scope='class')
+async def create_tracker_fixt(add_data_to_db, redis_cli) -> None:
+    tracker_obj = add_data_to_db
+    user_id = USER_ID_WITH_TRACKER_LIMIT
+    tracker = await redis_hmset_create_tracker(
+        user_id=user_id, tracker_id=tracker_obj.tracker_id,
+        action_id=tracker_obj.action_id, action_name='',
+        category_id=tracker_obj.category_id, category_name='', redis_client=redis_cli)
+    yield tracker
+    await redis_delete_tracker(user_id=user_id, redis_client=redis_cli)
