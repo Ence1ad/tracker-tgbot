306e3e5af4b4ad426f67eac434eb0a65e262a8b4
ence1ad <romanrusl@mail.ru>
2023-10-23 11:51:24 +0300

    fix(reports): Refactor report handling logic in weekly_report.py
    This commit refactors the logic in `tgbot/handlers/reports_handlers/weekly_report.py` for handling weekly reports. It updates the file name variable from `sheet_name` to `file_name` for clarity. The condition for updating the report has also been modified to check if `is_report_need_update` is `None` or `1` instead of just being a truthy value.
    
    Additionally, it ensures the correct type checking of `is_report_need_update` before proceeding with the report creation or retrieval.

--- tgbot/handlers/reports_handlers/weekly_report.py
+++ tgbot/handlers/reports_handlers/weekly_report.py
@@ -64,31 +64,32 @@ async def get_document(user_id, redis_client: Redis, db_session) -> FSInputFile 
     :return: FSInputFile | None: The user's report document as a file or None if no
      document is available.
     """
-    await redis_expireat_end_of_week(user_id, redis_client)
-    sheet_name = f"{settings.USER_REPORT_DIR}{user_id}/{settings.WEEKLY_XLSX_FILE_NAME}"
+    file_name = f"{settings.USER_REPORT_DIR}{user_id}/{settings.WEEKLY_XLSX_FILE_NAME}"
     is_report_need_update = await redis_get_report_need_upd(user_id, redis_client)
-    if is_report_need_update and int(is_report_need_update):
+
+    if (is_report_need_update is None) or int(is_report_need_update) == 1:
+        await redis_expireat_end_of_week(user_id, redis_client)
         await redis_set_report_need_upd(user_id, redis_client, value=0)
         return await create_document(user_id, db_session=db_session,
-                                     sheet_name=sheet_name)
+                                     file_name=file_name)
     else:
         # Get already exist report
-        if Path.exists(Path(sheet_name)):
-            document: FSInputFile = FSInputFile(sheet_name)
+        if Path.is_file(Path(file_name)):
+            document: FSInputFile = FSInputFile(file_name)
             return document
         else:
             return await create_document(user_id, db_session=db_session,
-                                         sheet_name=sheet_name)
+                                         file_name=file_name)
 
 
-async def create_document(user_id, db_session, sheet_name) -> FSInputFile | None:
+async def create_document(user_id, db_session, file_name) -> FSInputFile | None:
     """Create the user's weekly report document.
 
     This function generates and creates the user's weekly report document.
 
     :param user_id: The user's ID.
     :param db_session: The SQLAlchemy database session for database operations.
-    :param sheet_name: The name of the report sheet.
+    :param file_name: The name of the report file.
     :return: FSInputFile | None: The generated report document as a file or None if no
     report is available.
     """
@@ -97,8 +98,8 @@ async def create_document(user_id, db_session, sheet_name) -> FSInputFile | None
         action_data: DataFrame = await pd_action_data(report)
         category_data: DataFrame = await pd_category_data(report)
         await create_fig(df_action=action_data, df_categories=category_data,
-                         sheet_name=sheet_name)
-        document: FSInputFile = FSInputFile(sheet_name)
+                         file_name=file_name)
+        document: FSInputFile = FSInputFile(file_name)
         return document
     else:
         return
